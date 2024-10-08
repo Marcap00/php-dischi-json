593a6d3759e4d8f9a6ad4683e998328672b894ea
Marcap00 <cappellatomarco@yahoo.com>
2024-10-08 16:48:47 +0200

    made a call to api to get data and improve styles and fix layout

--- app/js/script.js
+++ app/js/script.js
@@ -3,7 +3,28 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            recordsList: [],
         }
+    },
+    methods: {
+        getRecords() {
+            axios.get('http://localhost/php-dischi-json/api/')
+                .then((response) => {
+                    console.log('Chiamata iniziata');
+                    // handle success
+                    console.log(response.data);
+                    this.recordsList = response.data;
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .finally(function () {
+                    console.log('Chiamata completata');
+                });
+        }
+    },
+    created() {
+        this.getRecords()
     }
 }).mount('#app')
